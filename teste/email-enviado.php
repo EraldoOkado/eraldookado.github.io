dbde06355b8c77bff8bf7bd89e4c0e42ab23918c
Eraldo Okado <eraldo.bmx@gmail.com>
2022-02-02 15:40:58 -0300

    first commit

---
+++ teste/email-enviado.php
@@ -0,0 +1,460 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<html>
+<head>
+	<title>Eraldo Okado - Web Developer</title>
+</head>
+<body>
+		<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script>
+  AOS.init();
+</script>
+<div class="email-enviado"><h1>O SEU EMAIL FOI ENVIADO COM SUCESSO.</h1></div>
+	<header>
+		<div data-aos="fade-right" class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: transparent !important;">
+  <a class="navbar-brand" href="index.php">Eraldo Okado</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-lg-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Início<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#sobre">Sobre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#servicos">Serviços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#trabalhos">Trabalhos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contato">Contato</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+		</div>
+	</header>
+	<section data-aos="zoom-in" class="banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<h2>SUA EMPRESA EM TODOS OS LUGARES</h2>
+					<p>Faça já o orçamento do seu negócio, e multiplique o número de vendas e acessos no site ou aplicativo da sua empresa, atraindo novos clientes e usuários. Somos especializados em Criação de Sites, Artes digitais, Aplicativos Mobile e SEO. Temos a menor duração de desenvolvimento de projeto, e cobrimos qualquer preço do mercado.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+						<div id="sobre">
+	<div data-aos="fade-up" class="sobre-container">
+	<div class="title-animated">
+		<div class="animated"><h1>Sobre</h1></div>
+		<div class="borderr"></div>
+	</div>
+	<div class="sobre">
+		<h2>Eraldo Okado</h2>
+		<p>Olá, sou Eraldo Inacio, mais conhecido como Eraldo Okado. Tenho 17 anos e estou finalizando o ensino médio, fiz curso de Técnico em Informática para Internet, HTML5 e CSS avançado, Javascript e Jquery básico, Photoshop e Illustrator intermediário, PHP e MySQL básico, Marketing Digital básico, SEO intermediário. Atuo na área há mais de 4 anos com credibilidade e parceria total com o cliente, com ideias e pensamentos flexíveis de acordo com a sua necessidade, para tornar o seu sonhado projeto uma realidade em sua vida ou empresa.</p>
+	</div>
+	<div class="habilidades">
+		<li>
+			<h3>HTML5</h3><span class="bar"><span class="html"></span></span>
+		</li>
+
+		<li>
+			<h3>CSS</h3><span class="bar"><span class="css"></span></span>
+		</li>
+
+		<li>
+			<h3>JAVASCRIPT</h3><span class="bar"><span class="js"></span></span>
+		</li>
+
+		<li>
+			<h3>JQUERY</h3><span class="bar"><span class="jquery"></span></span>
+		</li>
+
+		<li>
+			<h3>PHP</h3><span class="bar"><span class="php"></span></span>
+		</li>
+
+		<li>
+			<h3>PHOTOSHOP</h3><span class="bar"><span class="ps"></span></span>
+		</li>
+
+		<li>
+			<h3>ILLUSTRATOR</h3><span class="bar"><span class="ai"></span></span>
+		</li>
+	</div>
+	</div>
+	</div>
+	<div class="servicos">
+		<div id="servicos"></div>
+		<div class="title-animated">
+		<div class="animated"><h1>Serviços Mais Vendidos</h1></div>
+		<div class="borderr"></div>
+	</div>
+	<section>
+		<div class="container-fluid">
+			<div class="container-servicos">
+				<div class="row-servicos">
+					<div class="teste" data-aos="fade-up">
+						<div class="tabela-preco">
+							<div class="cabeca-preco">
+								<h4>Básico</h4>
+								<h2>R$ 500,00</h2>
+							</div>
+							<div class="preco-content">
+								<ul>
+								<li>Site Responsivo</li>
+								<li>Até 4 páginas</li>
+								<li>Estoque Multimídia</li>
+								<li>Sistema de Pesquisa
+								</li>
+								<li>Duração do projeto: 15 a 25 dias.</li>
+								</ul>
+							</div>
+							<div class="preco-botao">
+								<a href="#contato">Comprar</a>
+							</div>
+						</div>
+					</div>
+					<div class="teste" data-aos="fade-up">
+						<div class="tabela-preco">
+							<div class="cabeca-preco">
+								<h4>Intermediário</h4>
+								<h2>R$ 1000,00</h2>
+							</div>
+							<div class="preco-content">
+								<ul>
+								<li>Site Responsivo</li>
+								<li>Até 10 páginas</li>
+								<li>Sistema de gerenciamento de conteúdo</li>
+								<li>Trabalho de design de alta qualidade</li>
+								<li>Duração do projeto: 30 a 45 dias.</li>
+								</ul>
+							</div>
+							<div class="preco-botao">
+								<a href="#contato">Comprar</a>
+							</div>
+						</div>
+					</div>
+					<div class="teste" data-aos="fade-up">
+						<div class="tabela-preco">
+							<div class="cabeca-preco">
+								<h4>Avançado</h4>
+								<h2>R$ 2000,00</h2>
+							</div>
+							<div class="preco-content">
+								<ul>
+								<li>Sistema de E-commerce</li>
+								<li>Grenciamento de conteúdo</li>
+								<li>Painel de Controle Administrativo</li>
+								<li>Interatividade com o visitante</li>
+								<li>Duração do projeto: 45 a 90 dias.</li>
+								</ul>
+							</div>
+							<div class="preco-botao">
+								<a href="#contato">Comprar</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+</section></div>
+	<div id="trabalhos">
+<div class="title-animated" style="margin-top: -30px;">
+		<div class="animated"><h1>Principais Trabalhos</h1></div>
+		<div class="borderr"></div>
+	</div>
+	<div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/ondazul.jpg">
+      </div>
+      <div class="details">
+      	<h3>Ondazul Ecoturismo<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/preservarengenharia.jpg">
+      </div>
+      <div class="details">
+      	<h3>Preservar Engenharia<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/visaoengenharia.jpg">
+      </div>
+      <div class="details">
+      	<h3>Visão Engenharia<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/ecosup.jpg">
+      </div>
+      <div class="details">
+      	<h3>EcoSup<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/bdgmods.jpg">
+      </div>
+      <div class="details">
+      <h3>BDG MODS<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/incrivel-lixo.jpg">
+      </div>
+      <div class="details">
+      	<h3>O Incrível Lixo<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/familiasamp.jpg">
+      </div>
+      <div class="details">
+      	<h3>Família SAMP<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/texugomods.jpg">
+      </div>
+      <div class="details">
+      	<h3>Texugo MODS<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/speedcash.jpg">
+      </div>
+      <div class="details">
+      	<h3>Speed Cash<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/makeup.png">
+      </div>
+      <div class="details">
+      	<h3>Tayane Bezerra Makeup<br></h3>
+      </div>
+    </div>
+      <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/opdm.png">
+      </div>
+      <div class="details">
+      	<h3>O Ponto da Moda<br></h3>
+      </div>
+    </div>
+     <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/isabelly-modas.png">
+      </div>
+      <div class="details">
+      	<h3>Isabelly Modas<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/infinity-outlet.jpg">
+      </div>
+      <div class="details">
+      	<h3>Infinity Outlet<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/palazzo-panfleto.jpg">
+      </div>
+      <div class="details">
+      	<h3>Palazzo Pizzaria<br></h3>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <div class="imgBx">
+      	<img src="img/cardapio-happy.jpg">
+      </div>
+      <div class="details">
+      	<h3>Happy Lanches<br></h3>
+      </div>
+    </div>
+</div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+  </div>
+  </div>
+	<div class="title-animated">
+		<div class="animated"><h1>Feedback dos Clientes</h1></div>
+		<div class="borderr"></div>
+	</div>
+<div class="feedback">
+	<div class="feedback-container">
+		<div class="box" data-aos="fade-up">
+			<div class="imgBox">
+				<img src="https://www.altoastral.com.br/wp-content/uploads/2016/11/pessoa-assexuada.jpg" style="width: 200px;">
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.</p>
+			<h4>Isaac<br><span>Visão Engenharia</span></h4>
+		</div>
+		<div class="box" data-aos="fade-up">
+			<div class="imgBox">
+				<img src="https://www.altoastral.com.br/wp-content/uploads/2016/11/pessoa-assexuada.jpg" style="width: 200px;">
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.</p>
+			<h4>Fábio<br><span>Ondazul</span></h4>
+		</div>
+		<div class="box" data-aos="fade-up">
+			<div class="imgBox">
+				<img src="https://www.altoastral.com.br/wp-content/uploads/2016/11/pessoa-assexuada.jpg" style="width: 200px;">
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.</p>
+			<h4>Diogo<br><span>Preservar Engenharia</span></h4>
+		</div>
+		<div class="box" data-aos="fade-up">
+			<div class="imgBox">
+				<img src="img/tayane.jpg" style="width: 200px;    margin-left: -52px;
+    margin-top: -16px;">
+			</div>
+			<p>Trabalho incrível, super atencioso e muito cuidadoso, sempre me perguntando pelos detalhes para sair do jeitinho que eu queria, o resultado final ficou lindo e a minha cara.<br>Recomendo muito.</p>
+			<h4>Tayane<br><span>Makeup Tayane Bezerra</span></h4>
+		</div>
+		<div class="box" data-aos="fade-up">
+			<div class="imgBox">
+				<img src="https://www.altoastral.com.br/wp-content/uploads/2016/11/pessoa-assexuada.jpg" style="width: 200px;">
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.</p>
+			<h4>Isabel<br><span>Doces da Bel</span></h4>
+		</div>
+		<div class="box" data-aos="fade-up">
+			<div class="imgBox">
+				<img src="https://www.altoastral.com.br/wp-content/uploads/2016/11/pessoa-assexuada.jpg" style="width: 200px;">
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.</p>
+			<h4>BDG Mods<br><span>BDG MODS</span></h4>
+		</div>
+	</div>
+</div>
+	<div class="contato-section">
+		<div class="title-animated">
+		<div class="animated"><h1>Entre em Contato</h1></div>
+	</div>
+		<div id="contato">
+		<div class="borderr"></div>
+		<form class="contato-form" action="email/enviar.php" name="form_contato" method="post" >
+			<input type="text" class="contato-form-text" name="nome" placeholder="Nome" required>
+			<input type="email" class="contato-form-text" name="email" placeholder="Email" required>
+			<input type="text" class="contato-form-text" name="tel" placeholder="Telefone" required>
+			<textarea class="contato-form-text" name="msg" placeholder="Mensagem" required></textarea>
+			<input type="submit" class="contato-form-btn" name="BTEnvia" value="Enviar">
+		</form>
+	</div>
+	</div>
+	<!-- <div class="contato-rodape" data-aos="fade-up">
+	<div class="rodape-container">
+		<div class="rodape-box">
+			<div class="icon">
+				<i class="fa fa-map-marker" aria-hidden="true"></i>
+				<h4>Endereço</h4>
+				<h3>Brazil, AL</h3>
+			</div></div>
+			<div class="rodape-box">
+			<div class="icon">
+				<i class="fa fa-envelope" aria-hidden="true"></i>
+				<h4>Email</h4>
+				<h3>Contato@eraldookado.com</h3>
+			</div></div>
+			<div class="rodape-box">
+			<div class="icon">
+				<i class="fa fa-phone" aria-hidden="true"></i>
+				<h4>Telefone</h4>
+				<h3>82 98737-4323</h3>
+			</div></div>
+		</div>
+	</div> !-->
+	<footer id="footer">
+		<div class="container">
+			<div class="copyright">
+				&copy; 2019 All Rights Reserved.<br><strong>Eraldo Okado</strong>
+			</div>
+		</div>
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://kit.fontawesome.com/805ed6f851.js"></script>
+	<script type="text/javascript" src="js/swiper.min.js"></script>
+	<script>
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 60,
+        stretch: 0,
+        depth: 500,
+        modifier: 0.6,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  </script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <a href="#" class="scrollToTop"><i class="fas fa-chevron-up"></i></a>
+  <script type="text/javascript">
+  	$(document).ready(function(){
+  		//mostrar e ocultar botão
+  		$(window).scroll(function(){
+  			if($(this).scrollTop() > 500){
+  				$('.scrollToTop').fadeIn();
+  			} else {
+  				$('.scrollToTop').fadeOut();
+  			}
+  		});
+
+  		$('.scrollToTop').click(function(){
+  			$('html,body').animate({scrollTop: 0}, 100)
+  		})
+  	});
+  </script>
+</body>
+</html>
